5ad6360b55be5dbf20e768602f0af3e4c6035099
admin <admin@DESKTOP-JTMV4CP>
2024-03-21 12:26:34 +0900

    감사합니다 수정

--- javaPrograming/src/week3/ScannerEx.java
+++ javaPrograming/src/week3/ScannerEx.java
@@ -23,7 +23,7 @@ public class ScannerEx {
 		boolean isSingle = scanner.nextBoolean(); // 논리값 읽기
 		System.out.println("독신 여부는 " + isSingle + "입니다.");
 		
-		System.out.println("감사합니다");
+		System.out.println("감사합니다"); // 감사합니다
 		
 		scanner.close(); // scanner 객체 닫기
 		
