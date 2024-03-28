57b1e1a058789bb93c04782dcfc0498c11b1cb4a
admin <admin@DESKTOP-JTMV4CP>
2024-03-28 12:39:55 +0900

    4주차 깃

---
+++ javaPrograming/src/week4/week4.java
@@ -0,0 +1,21 @@
+package week4;
+
+public class week4 {
+
+	public static void main(String[] args) {
+
+		
+		
+		int i = 1;
+		i = i * 3;
+		
+		int j = 5;
+		j = i;
+		
+		
+		
+		System.out.println("i : " + i);
+		System.out.println("j : " + j);
+	}
+
+}
