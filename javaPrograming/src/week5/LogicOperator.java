da742130f33de9f3c528e3f7b8780ae520dfdc04
admin <admin@DESKTOP-JTMV4CP>
2024-04-03 11:09:44 +0900

    test

--- javaPrograming/src/week5/LogicOperator.java
+++ javaPrograming/src/week5/LogicOperator.java
@@ -17,6 +17,8 @@ public class LogicOperator {
 		System.out.println((3 > 2)&& (3 > 4));
 		System.out.println((3 != 2) || (-1 > 0));
 		System.out.println((3 != 2) ^ (-1 > 0));
+		
+		System.out.println("test");
 	}
 
 }
