8c6ff8ee45cbd2fb7c04fd5cb4235b124185c55f
admin <admin@DESKTOP-JTMV4CP>
2024-05-02 12:50:53 +0900

    8주차

---
+++ javaPrograming/src/week8/ContivueExample.java
@@ -0,0 +1,32 @@
+package week8;
+
+import java.util.Scanner;
+
+public class ContivueExample {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("정수를 5개 입력하세요.");
+		
+		int sum=0;
+		
+		for(int i=0; i<5; i++) 
+		{
+			int n = scanner.nextInt();
+			
+			if(n <= 0) 
+			{
+				continue;			// 양수가 아닌 경우 다음 반복으로 진행
+			}
+			else
+				sum += n;
+		}
+		System.out.println("양수의 합은 " + sum);
+		
+		scanner.close();
+		}
+	
+	}
+
+
